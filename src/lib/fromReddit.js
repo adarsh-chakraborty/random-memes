08ee7c38aa207185aeb28659134617bfaf730a44
Adarsh Chakraborty <adarshchak108@gmail.com>
2023-03-05 21:08:48 +0530

    some more changes

--- src/lib/fromReddit.js
+++ src/lib/fromReddit.js
@@ -6,14 +6,40 @@ var fromReddit = async function (subredditname) {
     'https://www.reddit.com/r/' + subredditname + '/hot/.json?count=100'
   );
   let memeObject = await response.json();
-  let postID = await memeObject.data.children[
-    Math.floor(Math.random() * memeObject.data.children.length)
-  ];
+
+  let finalMeme;
+  let attempts = 0;
+
+  while (!finalMeme && attempts < memeObject.data.children.length) {
+    let tempPost = await memeObject.data.children[
+      Math.floor(Math.random() * memeObject.data.children.length)
+    ];
+
+    const url = tempPost.data.url;
+    if (
+      url.includes('v.redd.it') ||
+      url.includes('i.redd.it') ||
+      url.includes('.jpg') ||
+      url.includes('.png') ||
+      url.includes('.gif') ||
+      url.includes('.jpeg') ||
+      url.includes('.webp') ||
+      url.includes('.mp4') ||
+      url.includes('.gifv')
+    ) {
+      finalMeme = tempPost;
+    }
+    attempts++;
+    console.log(
+      `Attempt ${attempts} of ${memeObject.data.children.length}...${url}`
+    );
+  }
+
   let meme = {
-    image: postID.data.url,
-    category: postID.data.link_flair_text,
-    caption: postID.data.title,
-    permalink: postID.data.permalink
+    image: finalMeme.data.url,
+    category: finalMeme.data.link_flair_text,
+    caption: finalMeme.data.title,
+    permalink: finalMeme.data.permalink
   };
 
   return meme;
